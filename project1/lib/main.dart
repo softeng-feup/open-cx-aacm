5e5784b53fb2f9ac4bb3fe13d0e45b896d1cb2b2
Andreia Gouveia <andiigouveia@hotmail.com>
2019-10-25 23:58:21 +0100

    Working with lists

--- project1/lib/main.dart
+++ project1/lib/main.dart
@@ -32,15 +32,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -49,27 +40,16 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-
+  List<String> listLectures=[];
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       if(_counter <12)
         _counter ++;
-    });
-  }
+    listLectures.add('Palestra ${_counter}');
+  });}
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -80,33 +60,20 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-         // mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>
           [
 
             for ( var i = 0; i < _counter; i++)
              new FlatButton.icon(
-              color: Colors.lightBlue, textColor: Colors.white,
+               padding: const EdgeInsets.all(10),
+              color: Colors.lightBlue,
+                 textColor: Colors.white,
               icon: Icon(Icons.favorite_border), //`Icon` to display
-              label: Text('Palestra ${i}'), //`Text` to display
+              label: Text(listLectures[i]), //`Text` to display
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => Forum(i)),
+                  MaterialPageRoute(builder: (context) => Forum(listLectures[i])),
                 );
               }
             )
@@ -125,8 +92,8 @@ class _MyHomePageState extends State<MyHomePage> {
 }
 
 class Forum extends StatelessWidget {
-  int j=0;
-  Forum(int i)
+  String j;
+  Forum(String i)
   {
     j=i;
   }
@@ -134,7 +101,7 @@ class Forum extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Forum Palestra ${j}"),
+        title: Text("Forum ${j}"),
       ),
       body: Center(
         child: RaisedButton(
